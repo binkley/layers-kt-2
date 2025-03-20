47634318bab626be4bea4013cff1b6a08fb8644c
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2025-03-20 10:11:00 -0500

    Copy over from layers-kt.

--- src/main/kotlin/hm/binkley/x/X.kt
+++ src/main/kotlin/hm/binkley/x/X.kt
@@ -1,3 +1,39 @@
 package hm.binkley.x
 
+import java.util.*
+
 fun main() = Unit
+
+sealed class ValueOrRule<V : Any> {
+    abstract override fun toString(): String
+}
+
+data class Value<T : Any>(
+    val value: T,
+) : ValueOrRule<T>() {
+    override fun toString() = "<Value>$value"
+}
+
+fun <T : Any> T.toValue() = Value(this)
+
+abstract class Rule<K : Any, T : Any>(
+    val name: String,
+) : ValueOrRule<T>(),
+    (K, Sequence<T>, Layers<K, T, *>) -> T {
+    override fun toString() = "<Rule>$name"
+}
+
+interface Layer<K : Any, V : Any, out L : Layer<K, V, L>> :
+    Map<K, ValueOrRule<V>> {
+    val name: String
+
+    @Suppress("UNCHECKED_CAST")
+    val self: L get() = this as L
+}
+
+interface Layers<K : Any, V : Any, L : Layer<K, V, L>> : Map<K, V> {
+    val name: String
+    val history: Stack<Layer<K, V, L>>
+
+    val current: Layer<K, V, L>
+}
