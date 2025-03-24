f83a6908537767a2a3fc5ab2145fdf268414c088
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2025-03-24 11:42:35 -0500

    Separate source file for rules

---
+++ src/main/kotlin/hm/binkley/layers/Rule.kt
@@ -0,0 +1,25 @@
+package hm.binkley.layers
+
+fun interface RuleFun<T : Any> : (Key, Sequence<T>, Layers) -> T
+
+open class Rule<T : Any>(
+    val name: String,
+    override val value: RuleFun<T>,
+) : Value<RuleFun<T>> {
+    override fun toString() = "<Rule>$name"
+
+    operator fun invoke(
+        key: Key,
+        values: Sequence<T>,
+        layers: Layers,
+    ) = value(key, values, layers)
+}
+
+inline fun <T : Any> rule(
+    name: String,
+    crossinline ruleFun: (Key, Sequence<T>, Layers) -> T,
+): Rule<T> = Rule(name) { key, values, layers -> ruleFun(key, values, layers) }
+
+/** The default rule unless another is given for a key. */
+fun <T : Any> mostRecentRule() =
+    rule<T>("<most recent>") { _, values, _ -> values.last() }
--- src/main/kotlin/hm/binkley/layers/Value.kt
+++ src/main/kotlin/hm/binkley/layers/Value.kt
@@ -11,27 +11,3 @@ data class IntValue(
 data class StringValue(
     override val value: String,
 ) : Value<String>
-
-fun interface RuleFun<T : Any> : (Key, Sequence<T>, Layers) -> T
-
-open class Rule<T : Any>(
-    val name: String,
-    override val value: RuleFun<T>,
-) : Value<RuleFun<T>> {
-    override fun toString() = "<Rule>$name"
-
-    operator fun invoke(
-        key: Key,
-        values: Sequence<T>,
-        layers: Layers,
-    ) = value(key, values, layers)
-}
-
-inline fun <T : Any> rule(
-    name: String,
-    crossinline ruleFun: (Key, Sequence<T>, Layers) -> T,
-): Rule<T> = Rule(name) { key, values, layers -> ruleFun(key, values, layers) }
-
-/** The default rule unless another is given for a key. */
-fun <T : Any> mostRecentRule() =
-    rule<T>("<most recent>") { _, values, _ -> values.last() }
